5bf626d64779c18d1187727d7f19b26285f3d15a
Prince Kumar <165674906+Princekumar-k21@users.noreply.github.com>
2025-10-05 22:14:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+//import WeatherApp from "./Weather_mini/WeatherApp";
+import WeatherApp from "./Practice";
+
+function App() {
+  return (
+   <div>
+    <WeatherApp/>
+   </div>
+  );
+}
+
+export default App;
